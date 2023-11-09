9b54e28b64a35416d02ec0d6d8ce97d957356c21
Alexandra Vásquez <40767818+alexve10@users.noreply.github.com>
2023-11-09 11:27:44 -0500

    Add files via upload

--- InterfazGrafica.py
+++ InterfazGrafica.py
@@ -2,12 +2,12 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-from PyQt5.QtCore import QIODevice
+from PyQt5.QtCore import QIODevice, Qt, QSize
+from PyQt5.QtGui import QPixmap, QIcon
 
 import pyqtgraph as pg
 import numpy as np
 import sys
-
 
 class MyApp(QMainWindow):
 
@@ -15,7 +15,43 @@ class MyApp(QMainWindow):
         super(MyApp, self).__init__()
         loadUi('InterfazV2.ui', self)
 
+        # Define la imagen de fondo
+        self.Imgfondo = QPixmap('FondoInterfazV5.png')
+        self.Imgfondo = self.Imgfondo.scaled(self.Fondo.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Fondo.setPixmap(self.Imgfondo)
+
+        #Inserción de Imagenes
+        self.Image1 = QPixmap('1.png')
+        self.Image2 = QPixmap('2.png')
+        self.Image3 = QPixmap('3.png')
+        self.Image4 = QPixmap('4.png')
+        self.Image5 = QPixmap('5.png')
+        self.Image6 = QPixmap('6.png')
+        self.Image7 = QPixmap('7.png')
+
+        self.Image1 = self.Image1.scaled(self.Punto1.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image2 = self.Image2.scaled(self.Punto2.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image3 = self.Image3.scaled(self.Punto3.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image4 = self.Image4.scaled(self.Punto4.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image5 = self.Image5.scaled(self.Punto5.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image6 = self.Image6.scaled(self.Punto6.size(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.Image7 = self.Image7.scaled(self.Punto7.size(), aspectRatioMode=Qt.KeepAspectRatio)
+
+        self.Punto1.setPixmap(self.Image1)
+        self.Punto2.setPixmap(self.Image2)
+        self.Punto3.setPixmap(self.Image3)
+        self.Punto4.setPixmap(self.Image4)
+        self.Punto5.setPixmap(self.Image5)
+        self.Punto6.setPixmap(self.Image6)
+        self.Punto7.setPixmap(self.Image7)
+
+        #Seleccion de Control
+        self.Control_Menu.currentIndexChanged.connect(self.CambioControl)
+        self.Control_Menu.addItem("Control Proporcional, Integral & Derivativo (PID)")
+        self.Control_Menu.addItem("Control Regulador Lineal Cuadrático (LQR)")
+
         self.flag_graf = "0"
+        self.flag_csv = "0"
         self.flag_check_R = "0"
         self.flag_check_P = "0"
         self.flag_check_Y = "0"
@@ -23,6 +59,18 @@ class MyApp(QMainWindow):
         self.flag_check_LC2 = "0"
         self.flag_check_LC3 = "0"
         self.flag_check_LC4 = "0"
+        self.flag_ControlMenu = "0"
+        self.flag_EnviarDatos = "0"
+
+        # Variables Medidas
+        self.Roll_Med = 0
+        self.Pitch_Med = 0
+        self.Yaw_Med = 0
+        self.M1_Med = 0
+        self.M2_Med = 0
+        self.M3_Med = 0
+        self.M4_Med = 0
+        self.acc = 0
 
         # Ganancias PID
         self.Val_R_Kp = 0.0
@@ -52,18 +100,36 @@ class MyApp(QMainWindow):
         self.Y_Set.valueChanged.connect(self.valor_cambiado)
 
         # Botones Generales
+        # Botón "Guardar"
+        self.setup_button(self.Guardar, "Guardar1.png", "Guardar2.png", "Guardar3.png", 208,40)
+
+        # Botón "Reset"
+        self.setup_button(self.Reset, "Reset1.png", "Reset2.png", "Reset3.png",208,42)
+
+        # Botón "Calibración"
+        self.setup_button(self.Calibrar, "Calibrar1.png", "Calibrar2.png", "Calibrar3.png",208,42)
+
         self.Guardar.clicked.connect(self.guardar_valores)
         self.Reset.clicked.connect(self.reset_valores)
         #self.Calibrar.clicked.connect(self.calibrar_sensor)
 
         # Control Connect
+        self.setup_button(self.Actualizar, "Actualizar1.png", "Actualizar2.png", "Actualizar3.png",209,23)
+
+        # Botón "Reset"
+        self.setup_button(self.Conectar, "Conectar1.png", "Conectar2.png", "Conectar3.png",209,23)
+
+        # Botón "Calibración"
+        self.setup_button(self.Desconectar, "Desconectar1.png", "Desconectar2.png", "Desconectar3.png",209,23)
+
         self.serial = QSerialPort()
         self.Actualizar.clicked.connect(self.read_ports)
         self.Conectar.clicked.connect(self.serial_connect)
         self.Desconectar.clicked.connect(lambda: self.serial.close())
 
         self.serial.readyRead.connect(self.read_data)
-        self.Acelerador.valueChanged.connect(self.accelerator_pwm)
+        # self.Acelerador.valueChanged.connect(self.accelerator_pwm)
+        self.Acelerador.sliderReleased.connect(self.accelerator_pwm)
 
         self.x1 = list(np.linspace(0, 100, 100))
         self.x2 = list(np.linspace(0, 100, 100))
@@ -81,6 +147,10 @@ class MyApp(QMainWindow):
         self.y7 = list(np.linspace(0, 0, 100))
 
         # Grafica
+        self.setup_button(self.Inicio, "Iniciar1.png", "Iniciar2.png", "Iniciar3.png", 106, 24)
+        # Botón "Reset"
+        self.setup_button(self.Parar, "Parar1.png", "Parar2.png", "Parar3.png", 106, 24)
+
         self.Inicio.clicked.connect(self.iniciar_grafica)
         self.Parar.clicked.connect(self.parar_grafica)
         pg.setConfigOption('background', '#ffffff')
@@ -99,6 +169,34 @@ class MyApp(QMainWindow):
 
         self.read_ports()
 
+    def setup_button(self, button, normal_image_path, hover_image_path, clicked_image_path, Size1, Size2):
+        # Establecer la imagen normal
+        normal_image = QPixmap(normal_image_path)
+        normal_image = normal_image.scaled(QSize(Size1, Size2))
+        button.setIcon(QIcon(normal_image))
+        button.setIconSize(QSize(Size1, Size2))
+
+        # Establecer la imagen cuando el cursor está sobre el botón
+        hover_image = QPixmap(hover_image_path)
+        hover_image = hover_image.scaled(QSize(Size1, Size2))
+
+        # Establecer la imagen cuando está clickeado
+        clicked_image = QPixmap(clicked_image_path)
+        clicked_image = clicked_image.scaled(QSize(Size1, Size2))
+
+        # Conectar señales a funciones
+        button.enterEvent = lambda event, b=button, h=hover_image: self.enter_button(event, b, h)
+        button.leaveEvent = lambda event, b=button, n=normal_image: self.leave_button(event, b, n)
+        button.clicked.connect(lambda: self.clicked_button(button, clicked_image))
+
+    def enter_button(self, event, button, hover_image):
+        button.setIcon(QIcon(hover_image))
+
+    def leave_button(self, event, button, normal_image):
+        button.setIcon(QIcon(normal_image))
+
+    def clicked_button(self, button, clicked_image):
+        button.setIcon(QIcon(clicked_image))
     def guardar_valores(self):
         print("Valor de Kp Roll:", self.Val_R_Kp)
         print("Valor de Kp Pitch:", self.Val_P_Kp)
@@ -113,6 +211,8 @@ class MyApp(QMainWindow):
         print("Valor de Setpoint Pitch:", self.Val_P_Set)
         print("Valor de Setpoint Yaw:", self.Val_Y_Set)
 
+        self.flag_EnviarDatos = "2"
+        self.send_data()
     def reset_valores(self):
         Val_R_Kp, Val_P_Kp, Val_Y_Kp = 0.0, 0.0, 0.0
         Val_R_Ki, Val_P_Ki, Val_Y_Ki = 0.0, 0.0, 0.0
@@ -231,9 +331,18 @@ class MyApp(QMainWindow):
 
         # Convertir los valores a números flotantes
         y1, y2, y3, y4, y5, y6, y7 = map(float, data_list)
+        self.Roll_Med = y1
+        self.Pitch_Med = y2
+        self.Yaw_Med = y3
+        self.M1_Med = y4
+        self.M2_Med = y5
+        self.M3_Med = y6
+        self.M4_Med = y7
+
+        # print(f"Valor 1: {self.Roll_Med}, Valor 2: {self.Pitch_Med}, Valor 3: {self.Yaw_Med}, Valor 4: {self.M1_Med}, Valor 5: {self.M2_Med}, Valor 6: {self.M3_Med}, Valor 7: {self.M4_Med}")
 
         # Actualizar gráficas según las casillas de verificación
-        plot_colors = ['#da0037', '#FF5733', '#BC970B', '#28A70E', '#109772', '#106697', '#106697']
+        plot_colors = ['#da0037', '#FF5733', '#BC970B', '#28A70E', '#109772', '#106697', '#4F1097']
         plots = []
 
         if self.flag_graf == "1":
@@ -275,40 +384,81 @@ class MyApp(QMainWindow):
     def parar_grafica(self):
         self.flag_graf = "0"
 
-    def send_data(self, data):
+    def guardar_csv(self):
+        if (self.flag_graf = "1"):
+
+        if (self.flag_graf = "0"):
+
+    def send_data(self):
+        if(self.flag_EnviarDatos == "1"):
+            #Para enviar solo acelerador
+            data = str(self.flag_EnviarDatos) + "," + str(self.acc)
+
+        if(self.flag_EnviarDatos == "2"):
+            #Para enviar valores de ganancias
+            data = (str(self.flag_EnviarDatos) + "," + str(self.Val_R_Kp) + "," + str(self.Val_R_Ki) + "," + str(self.Val_R_Kd) + "," + str(self.Val_P_Kp) + "," + str(self.Val_P_Ki) + "," + str(self.Val_P_Kd) + "," + str(self.Val_Y_Kp) + "," + str(self.Val_Y_Ki) + "," + str(self.Val_Y_Kd) + "," + str(self.Val_R_Set) + "," + str(self.Val_P_Set) + "," + str(self.Val_Y_Set))
+
         data = data + "\n"
         print(data)
+
         if self.serial.isOpen():
             self.serial.write(data.encode())
 
-    def accelerator_pwm(self, event):
-        self.Acelerador.setValue(event)
-        self.val_Acc.setText(str(event))
-        acc = '1,' + str(event)
-        self.send_data(acc)
+    def accelerator_pwm(self):
+        self.acc = self.Acelerador.value()
+        self.flag_EnviarDatos = "1"
+        self.send_data()
+
+        self.val_Acc.setAlignment(Qt.AlignCenter)
+        self.val_Acc.setText(str(self.acc))
+
+    ''' def accelerator_pwm(self, event):
+        if self.serial.isOpen():
+            self.Acelerador.setValue(event)
+            self.val_Acc.setText(str(event))
+            # acc = '1,' + str(event)
+            self.acc = str(event)
+            #self.send_data(acc)
+            self.flag_EnviarDatos = "1"
+            self.send_data()
+
+            self.val_Acc.setAlignment(Qt.AlignCenter)
+            self.val_Acc.setText(str(event))
+        '''
 
     def PWM_motores(self):
-        Val_PWM_M1 = 1100
-        Val_PWM_M2 = 1200
-        Val_PWM_M3 = 1400
-        Val_PWM_M4 = 1500
-        self.M1.setValue(Val_PWM_M1)
-        self.M2.setValue(Val_PWM_M2)
-        self.M3.setValue(Val_PWM_M3)
-        self.M4.setValue(Val_PWM_M4)
+        Val_PWM_M1 = self.M1_Med
+        Val_PWM_M2 = self.M2_Med
+        Val_PWM_M3 = self.M3_Med
+        Val_PWM_M4 = self.M4_Med
+        self.M1.setValue(int(Val_PWM_M1))
+        self.M2.setValue(int(Val_PWM_M2))
+        self.M3.setValue(int(Val_PWM_M3))
+        self.M4.setValue(int(Val_PWM_M4))
 
     def actualizar_valores(self):
-        self.Roll_Medido = 0.3
-        self.Pitch_Medido = 0.5
-        self.Yaw_Medido = 5
-
-        Roll_Medido = str(self.Roll_Medido)
-        Pitch_Medido = str(self.Pitch_Medido)
-        Yaw_Medido = str(self.Yaw_Medido)
+        Roll_Medido = str(self.Roll_Med)
+        Pitch_Medido = str(self.Pitch_Med)
+        Yaw_Medido = str(self.Yaw_Med)
 
         self.Med_Roll.setText(Roll_Medido)
         self.Med_Pitch.setText(Pitch_Medido)
         self.Med_Yaw.setText(Yaw_Medido)
+
+    def CambioControl(self):
+        seleccion = self.Control_Menu.itemText(self.Control_Menu.currentIndex())
+
+        if seleccion == "Control Proporcional, Integral & Derivativo (PID)":
+            self.Kp.setText("Kp")
+            self.Ki.setText("Ki")
+            self.Kd.setText("Kd")
+            self.flag_ControlMenu = "1"
+
+        if seleccion == "Control Regulador Lineal Cuadrático (LQR)":
+            self.Kp.setText("Ki")
+            self.Ki.setText("K1")
+            self.Kd.setText("K2")
+            self.flag_ControlMenu = "2"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
